b9441fc1d97706c7c179215b42752b9db0af1b2f
NeverCry <dima_panchenko_2222@vk.com>
2025-03-04 19:59:16 +0500

    чут чут оптимизироовал

--- dice/coin.py
+++ dice/coin.py
@@ -1,12 +1,10 @@
 import random
 
-async def coin_f():
+async def coin_f() -> str:
 	coin_res = random.randint(1, 2)
 	if coin_res == 1:
-		mes = "CAACAgIAAxkBAAEI2-Vm-rfm9Qtgxa1WyM_0psS4sIvtfgACVAADUqjXBMz71bIdmgEhNgQ"
 		print("Орёл / Дракон")
-		return mes
+		return "CAACAgIAAxkBAAEI2-Vm-rfm9Qtgxa1WyM_0psS4sIvtfgACVAADUqjXBMz71bIdmgEhNgQ"
 	else:
-		mes = "CAACAgIAAxkBAAEI2-dm-rfohfhUhdZxWYTQRRvqxfY7XgACVQADUqjXBGVDO3J4yuC9NgQ"
 		print("Решка / Штурвал")
-		return mes
+		return "CAACAgIAAxkBAAEI2-dm-rfohfhUhdZxWYTQRRvqxfY7XgACVQADUqjXBGVDO3J4yuC9NgQ"
--- dice/rolldice.py
+++ dice/rolldice.py
@@ -2,26 +2,19 @@ import random
 from dice.phrase import *
 
 class Roll:
-	def __init__(self, min: int, max: int):
+	def __init__(self, min: int, max: int) -> None: 
 		self.min = min
 		self.max = max
 		
-	async def roll(self, miss, crit):
+	async def roll(self, miss, crit) -> str:
 		res = random.randint(self.min, self.max)	
-		if res < miss:
-			chose_miss = random.choice(miss_phrase)
-			mes = f"{chose_miss}|{res}|"
-			return mes
-		
-		elif res > crit:
-			chose_crit = random.choice(crit_phrase)
-			mes = f"{chose_crit}|{res}|"
-			return mes
+		if res < miss: return f"{random.choice(miss_phrase)}|{res}|"
+						
+		elif res > crit: return f"{random.choice(crit_phrase)}|{res}|"	
 
-		else:
-			chose_standart = random.choice(standart_phrase)
-			mes = f"{chose_standart}|{res}|"
-			return mes	
+		else: return f"{random.choice(standart_phrase)}|{res}|"
+				 
+				
 
 		
 
